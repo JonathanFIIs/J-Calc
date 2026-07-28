349a23fcc4323b0cc8791eb567afd980fee6be4c
JonathanFIIs <129765036+JonathanFIIs@users.noreply.github.com>
2026-07-28 18:00:05 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,10 @@
-const CACHE_NAME = 'jcalc-v3';
+const CACHE_NAME = 'jcalc-v4';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
   'https://cdn.jsdelivr.net/npm/chart.js',
   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css'
 ];
