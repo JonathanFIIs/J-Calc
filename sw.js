0984f2ae6e3917f7deea7dcaf3d03d2caddf6e36
JonathanFIIs <129765036+JonathanFIIs@users.noreply.github.com>
2026-07-28 17:16:41 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'jcalc-v1';
+const CACHE_NAME = 'jcalc-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -8,9 +8,24 @@ const ASSETS_TO_CACHE = [
 ];
 
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
     })
   );
 });
